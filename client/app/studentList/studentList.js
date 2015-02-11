aeb9850c20be9adab81a9ee927ddec5cebc72b56
Jorrobo <lynch446@morris.umn.edu>
2015-02-10 18:12:36 -0600

    checking problems with lemmon, getting into linking for navbar and index.

--- client/app/studentList/studentList.js
+++ client/app/studentList/studentList.js
@@ -1,3 +1,14 @@
 /**
  * Created by lynch446 on 2/10/15.
  */
+'use strict';
+
+angular.module('jsonDataProcessingLabJackMollyLemmonApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
