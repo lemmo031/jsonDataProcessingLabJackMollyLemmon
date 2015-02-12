4a3227bc491edd374484d3202bd23d62261a29a7
Jorrobo <lynch446@morris.umn.edu>
2015-02-12 12:49:17 -0600

    still getting student object created.

--- server/api/student/student.model.js
+++ server/api/student/student.model.js
@@ -1,3 +1,18 @@
 /**
  * Created by grove266 on 2/10/15.
  */
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var StudentSchema = new Schema({
+
+  //need to create the student model from our existing seed.
+
+  //name: String,
+  //info: String,
+  //active: Boolean
+});
+
+module.exports = mongoose.model('Student', StudentSchema);
--- server/api/student/student.socket.js
+++ server/api/student/student.socket.js
@@ -1,3 +1,23 @@
 /**
  * Created by lynch446 on 2/12/15.
  */
+'use strict';
+
+var student = require('./student.model');
+
+exports.register = function(socket) {
+  student.schema.post('save', function (doc) {
+    onSave(socket, doc);
+  });
+  student.schema.post('remove', function (doc) {
+    onRemove(socket, doc);
+  });
+}
+
+function onSave(socket, doc, cb) {
+  socket.emit('student:save', doc);
+}
+
+function onRemove(socket, doc, cb) {
+  socket.emit('student:remove', doc);
+}
