c95b85d2e530a39fdd4c7e28a7d3e39c73a46094
Jorrobo <lynch446@morris.umn.edu>
2015-02-12 12:34:27 -0600

    getting student object created. finished controller.

--- server/api/student/student.controller.js
+++ server/api/student/student.controller.js
@@ -34,23 +34,23 @@ exports.create = function(req, res) {
 // Updates an existing thing in the DB.
 exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
-  Student.findById(req.params.id, function (err, thing) {
+  Student.findById(req.params.id, function (err, student) {
     if (err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
-    var updated = _.merge(thing, req.body);
+    if(!student) { return res.send(404); }
+    var updated = _.merge(student, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
-      return res.json(200, thing);
+      return res.json(200, student);
     });
   });
 };
 
 // Deletes a thing from the DB.
 exports.destroy = function(req, res) {
-  Student.findById(req.params.id, function (err, thing) {
+  Student.findById(req.params.id, function (err, student) {
     if(err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
-    thing.remove(function(err) {
+    if(!student) { return res.send(404); }
+    student.remove(function(err) {
       if(err) { return handleError(res, err); }
       return res.send(204);
     });
