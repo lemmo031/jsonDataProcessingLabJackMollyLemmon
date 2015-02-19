4f6f17710fcc5b2a054bb9a956a855e6373d71db
Jorrobo <lynch446@morris.umn.edu>
2015-02-19 13:46:58 -0600

    creating new details in our initial html so we don't have to dance with another page.

--- client/app/studentDetails/studentDetails.controller.js
+++ client/app/studentDetails/studentDetails.controller.js
@@ -6,26 +6,28 @@
 
 angular.module('jsonDataProcessingLabJackMollyLemmonApp')
   .controller('StudentDetailsCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.awesomeStudents = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+
+
+    $http.get('/api/students').success(function(awesomeStudents) {
+      $scope.awesomeStudents = awesomeStudents;
+      socket.syncUpdates('student', $scope.awesomeStudents);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    //$scope.addStudent = function() {
+    //  if($scope.newStudent === '') {
+    //    return;
+    //  }
+    //  $http.post('/api/students', { name: $scope.newStudent });
+    //  $scope.newStudent = '';
+    //};
+    //
+    //$scope.deleteStudent = function(student) {
+    //  $http.delete('/api/students/' + student._id);
+    //};
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('student');
     });
   });
